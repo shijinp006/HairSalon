d29a4f741a1e048e92b4a8c394e89af7a6efd622
Shijin P <shijinp006@gmail.com>
2026-06-16 10:19:40 +0530

    first commit

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -6,7 +6,7 @@ import babel from '@rolldown/plugin-babel'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react()
     tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
